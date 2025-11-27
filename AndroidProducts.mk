f382c23ddd9193bf2de9dca8322f7ba635f9a19e
JoeyKoala07 <128089786+JoeyKoala07@users.noreply.github.com>
2025-11-27 13:41:16 -0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,6 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_Asteroids.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_Asteroids-eng
+    twrp_Asteroids-eng \
+    twrp_Asteroids-userdebug
