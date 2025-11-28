b54e81213aacb6c19e28fa49b62037da273de0ec
JoeyKoala07 <128089786+JoeyKoala07@users.noreply.github.com>
2025-11-28 00:15:37 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+#!/bin/bash
+add_lunch_combo twrp_Asteroids-eng
+add_lunch_combo omni_Asteroids-eng
 add_lunch_combo omni_Asteroids-user
 add_lunch_combo omni_Asteroids-userdebug
-add_lunch_combo omni_Asteroids-eng
