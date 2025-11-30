e1ae908f3be526449aaef0d3ce70ddbb8caaaa69
JoeyKoala07 <128089786+JoeyKoala07@users.noreply.github.com>
2025-11-30 14:24:08 -0500

    Update omni_Asteroids.mk

--- omni_Asteroids.mk
+++ omni_Asteroids.mk
@@ -1,6 +1,4 @@
-#
 # Omni product definition for Nothing Phone 3a (Asteroids)
-#
 
 PRODUCT_RELEASE_NAME := Asteroids
 PRODUCT_DEVICE       := Asteroids
@@ -9,13 +7,13 @@ PRODUCT_BRAND        := Nothing
 PRODUCT_MODEL        := Nothing Phone 3a
 PRODUCT_MANUFACTURER := Nothing
 
-# Match your current Android 15 build (3.2 - 250924-1736)
+# Match your Android 15 build (3.2 - 250924-1736)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="Asteroids-user 15 AQ3A.241015.001 2509241736 release-keys"
 
 BUILD_FINGERPRINT := Nothing/Asteroids/Asteroids:15/AQ3A.241015.001/2509241736:user/release-keys
 
-# Inherit common 64-bit telephony configuration
+# Inherit common config
 $(call inherit-product, build/target/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
