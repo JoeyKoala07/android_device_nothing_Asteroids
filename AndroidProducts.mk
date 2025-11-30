6236300b17b86a43a01c25b4612087f925e1b4d6
JoeyKoala07 <128089786+JoeyKoala07@users.noreply.github.com>
2025-11-30 02:30:28 -0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,11 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_Asteroids.mk
+LOCAL_PATH := $(call my-dir)
 
+# List all product makefiles for this device
+PRODUCT_MAKEFILES := \
+    $(LOCAL_PATH)/omni_Asteroids.mk \
+    $(LOCAL_PATH)/twrp_Asteroids.mk
+
+# Modern lunch targets (replaces old add_lunch_combo)
 COMMON_LUNCH_CHOICES := \
-    omni_Asteroids-user \
-    omni_Asteroids-userdebug \
-    omni_Asteroids-eng
+    omni_Asteroids-eng \
+    twrp_Asteroids-eng
