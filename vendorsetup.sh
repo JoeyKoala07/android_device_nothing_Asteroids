497f04751eaf360d26a0b07b93a7675eaacd20e7
JoeyKoala07 <128089786+JoeyKoala07@users.noreply.github.com>
2025-11-30 04:45:59 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,11 @@
-#!/bin/true
-# empty because COMMON_LUNCH_CHOICES is used instead
+#
+# Vendor setup for Nothing Phone 3a (Asteroids)
+#
+
+add_lunch_combo omni_Asteroids-eng
+add_lunch_combo omni_Asteroids-userdebug
+add_lunch_combo omni_Asteroids-user
+
+add_lunch_combo twrp_Asteroids-eng
+add_lunch_combo twrp_Asteroids-userdebug
+add_lunch_combo twrp_Asteroids-user
