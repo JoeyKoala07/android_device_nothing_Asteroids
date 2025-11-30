a4591e9f884aa8558f481606f01a9aa5e641f57e
JoeyKoala07 <128089786+JoeyKoala07@users.noreply.github.com>
2025-11-30 04:44:12 -0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,10 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 LOCAL_PATH := $(call my-dir)
 
 # List product makefiles for this device
@@ -5,7 +12,11 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_PATH)/omni_Asteroids.mk \
     $(LOCAL_PATH)/twrp_Asteroids.mk
 
-# Modern lunch targets (replaces legacy add_lunch_combo)
+# Modern lunch targets (replaces legacy add_lunch_combo usage)
 COMMON_LUNCH_CHOICES := \
+    omni_Asteroids-user \
+    omni_Asteroids-userdebug \
     omni_Asteroids-eng \
+    twrp_Asteroids-user \
+    twrp_Asteroids-userdebug \
     twrp_Asteroids-eng
