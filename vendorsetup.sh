a12aa6a37a0fccca591b69968743d701e0b3886e
JoeyKoala07 <128089786+JoeyKoala07@users.noreply.github.com>
2025-11-30 03:32:10 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,2 @@
-#!/bin/bash
-#
-# vendorsetup.sh – legacy stub for Asteroids
-#
-# Modern AOSP/OrangeFox uses COMMON_LUNCH_CHOICES in AndroidProducts.mk.
-# We intentionally keep this file empty to avoid add_lunch_combo warnings.
-#
-# Do not add add_lunch_combo here.
-
-return 0 2>/dev/null || exit 0
+#!/bin/true
+# empty because COMMON_LUNCH_CHOICES is used instead
