f22c8dddae9761d38c716111e4ae7301f8b31f2d
JoeyKoala07 <128089786+JoeyKoala07@users.noreply.github.com>
2025-11-30 04:45:33 -0500

    Update twrp_Asteroids.mk

--- twrp_Asteroids.mk
+++ twrp_Asteroids.mk
@@ -1,4 +1,6 @@
-# TWRP / OrangeFox product definition for Nothing Phone 3a (Asteroids)
+#
+# TWRP / recovery product definition for Nothing Phone 3a (Asteroids)
+#
 
 PRODUCT_RELEASE_NAME := Asteroids
 PRODUCT_DEVICE       := Asteroids
@@ -7,15 +9,14 @@ PRODUCT_BRAND        := Nothing
 PRODUCT_MODEL        := Nothing Phone 3a
 PRODUCT_MANUFACTURER := Nothing
 
+# Same build description / fingerprint as omni product
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="Asteroids-user 15 AQ3A.241015.001 2509241736 release-keys"
 
 BUILD_FINGERPRINT := Nothing/Asteroids/Asteroids:15/AQ3A.241015.001/2509241736:user/release-keys
 
-PRODUCT_GMS_CLIENTID_BASE := android-nothing
-
-# Inherit common 64-bit telephony configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+# Inherit common config
+$(call inherit-product, build/target/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit device-specific configuration
